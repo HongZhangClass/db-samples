645fdb35ab194411cd12f6b58f178645b7355ea1
Jeremy Hajek <hajek@iit.edu>
2017-02-06 09:29:45 -0600

    Created example sample insert statement

---
+++ insert.sql
@@ -0,0 +1,4 @@
+USE store; 
+
+INSERT INTO items (id, email, phone, filename, s3rawurl, s3finishedurl, status, issubscribed) 
+VALUES (0,"you@hawk.iit.edu","312-555-5678","bestfile.ever","http://raw.example.com","http://finished.example.com",0,0);
